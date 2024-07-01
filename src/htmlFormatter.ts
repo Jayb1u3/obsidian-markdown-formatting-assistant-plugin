8bdb6879c2766a9fde336bcf227e53fe0392d078
Jayb1u3 <120038570+Jayb1u3@users.noreply.github.com>
2024-07-01 00:48:51 -0400

    Update htmlFormatter.ts

--- src/htmlFormatter.ts
+++ src/htmlFormatter.ts
@@ -8,6 +8,127 @@ export interface htmlFormatterSetting extends baseFormatterSetting {
 }
 
 export const htmlFormatterSettings = {
+  asyncFunction: {
+    des: "Async Function",
+    symbol: 'async function functionName() {\\n}',
+    shift: 31,  // Adjusts cursor inside the function body
+    selectionInput: 30,  // Positions cursor at the start of the function body
+    objectType: "htmlFormatterSetting",
+  },  
+  appendText: {
+    des: "Append Text",
+    symbol: "tR += ",
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  templaterJSTags: {
+    des: "<%* %>",
+    symbol: "<%* %> ",
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  templaterTags: {
+    des: "<% %>",
+    symbol: "<% %>",
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  sp: {
+    des: "<system.prompt>",
+    symbol: '<% tp.system.prompt("") %>',
+    shift: 21,
+    selectionInput: 21,
+    objectType: "htmlFormatterSetting",
+  },
+  suggesterFunction: {
+    des: "Suggester Function",
+    symbol: `const choice = await tp.system.suggester(["Option1", "Option2"], ["Option1", "Option2"]);`,
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  suggesterFunction: {
+    des: "Suggest Array",
+    symbol: `const suggestions = ["1", "2", "3"]; 
+    const suggestions = await tp.system.suggester(suggestions, suggestions, { 
+        placeholder: "Select a view type" });
+        
+        //Check if  the user made a selection and respond accordingly
+        if (suggestions) {
+          tR += suggestions; //append the selected  option to the document
+        } else {
+          console.log("No option was selected."); //Log  to console if no selection was made
+        }`,
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  templateLiteral: {
+    des: "${}",
+    symbol: "${}",
+    objectType: "htmlFormatterSetting",
+  },
+  taskStatus: {
+    des: "Task Status",
+    symbol:
+      '<% tp.system.suggester(["Complete", "Incomplete"], ["Complete", "Incomplete"]) %>',
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  date: {
+    des: "Current Date",
+    symbol: '<% tp.date.now("YYYY-MM-DD") %>',
+    shift: 0, // Adjust these values based on your usage
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  dateTime: {
+    des: "Current Date and Time",
+    symbol: '<% tp.date.now("YYYY-MM-DD HH:mm") %>',
+    shift: 0, // Adjust accordingly
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  renameFile: {
+    des: "Rename File",
+    symbol: 'await tp.file.rename("New File Name");',
+    shift: 0, // Adjust accordingly
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  arrayDeclaration: {
+    des: "Array Declaration",
+    symbol: "const myArray = [element1, element2, element3];",
+    objectType: "htmlFormatterSetting",
+  },
+  FMTitle: {
+    des: "Frontmatter Title",
+    symbol: "<% tp.frontmatter['title'] %>",
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  fileTitle: {
+    des: "File Title",
+    symbol: "<% tp.file.title %>",
+    shift: 0,
+    selectionInput: 0,
+    objectType: "htmlFormatterSetting",
+  },
+  promptInput: {
+    des: "Prompt for Input",
+    symbol: 'const input = await tp.system.prompt("Enter your input");',
+    objectType: "htmlFormatterSetting",
+  },
+  newLine: {
+    des: "New Line",
+    symbol: "\\n",
+    objectType: "htmlFormatterSetting",
+  },
   br: {
     des: '<br/>',
     symbol: '<br/>',
@@ -49,15 +170,73 @@ export const htmlFormatterSettings = {
     shift: 3,
     selectionInput: 3,
     objectType: 'htmlFormatterSetting',
-  },
+  },  
   font: {
-    des: '<font>',
+    des: "<font>",
     symbol:
-      '<span style="font-family:default; font-size:default; color:red"></span>',
-    shift: 64,
-    selectionInput: 64,
-    objectType: 'htmlFormatterSetting',
+      '<span style="font-family:inherit; font-size:inherit; font-weight:inherit; font-style:inherit; text-decoration:inherit; color:inherit; text-shadow: 0px 0px 0px rgba(0,0,0,0.5);"></span>',
+    shift: 177,
+    selectionInput: 177,
+    objectType: "htmlFormatterSetting",
   },
+  fontStyleCustomization: {
+    des: "<font Enh>",
+      symbol: `<%*
+    async function customizeFontStyle() {
+        try {
+            const innerText = await tp.system.prompt("Enter the text to style:");
+            if (!innerText) {
+                new Notice("No text entered. Exiting script.");
+                return;
+            }
+    
+            let styleChanges = 'text-shadow: 0px 0px 0px rgba(0,0,0,0.5);';
+    
+            const fontWeight = ["Bold", "Normal"];
+            const fontStyle = ["Italic", "Normal"];
+            const textDecoration = ["Underline", "None"];
+    
+            const selectedFontWeight = await tp.system.suggester(fontWeight, fontWeight, {
+                placeholder: "Choose font weight:"
+            });
+            if (selectedFontWeight === "Bold") {
+                styleChanges += ' font-weight: bold;';
+            }
+    
+            const fontColor = await tp.system.prompt("Enter font color (or leave blank):");
+            if (fontColor) {
+                styleChanges += \` color: \${fontColor};\`;
+            }
+    
+            const selectedFontStyle = await tp.system.suggester(fontStyle, fontStyle, {
+                placeholder: "Choose font style:"
+            });
+            if (selectedFontStyle) {
+                styleChanges += \` font-style: \${selectedFontStyle.toLowerCase()};\`;
+            }
+    
+            const selectedTextDecoration = await tp.system.suggester(textDecoration, textDecoration, {
+                placeholder: "Choose text decoration:"
+            });
+            if (selectedTextDecoration) {
+                styleChanges += \` text-decoration: \${selectedTextDecoration.toLowerCase()};\`;
+            }
+    
+            const spanTag = \`<span style="\${styleChanges}">\${innerText}</span>\`;
+    
+            tR += spanTag;
+            new Notice("Styled text configured successfully.");
+        } catch (error) {
+            console.error("Error in customizeFontStyle:", error);
+            new Notice("An error occurred: " + error.message);
+        }
+    }
+    await customizeFontStyle();
+    %>`,
+      shift: 0,
+      selectionInput: 0,
+      objectType: "htmlFormatterSetting",
+    },
   table: {
     des: '<table>',
     symbol: '<table></table>',
@@ -127,6 +306,20 @@ export const htmlFormatterSettings = {
     shift: 3,
     selectionInput: 3,
     objectType: 'htmlFormatterSetting',
+  },
+  sup: {
+    des: "<sup>",
+    symbol: "<sup></sup>",
+    shift: 5,
+    selectionInput: 5,
+    objectType: "htmlFormatterSetting",
+  },
+  sub: {
+    des: "<sub>",
+    symbol: "<sub></sub>",
+    shift: 5,
+    selectionInput: 5,
+    objectType: "htmlFormatterSetting",
   },
 };
 
